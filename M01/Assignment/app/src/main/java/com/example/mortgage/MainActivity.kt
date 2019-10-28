f9335981fc3c1f0de16373ac911275a0c0e4a50f
Tyler Berrett <btylerberrett@gmail.com>
2019-10-28 13:54:42 -0600

    initial commit

---
+++ M01/Assignment/app/src/main/java/com/example/mortgage/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mortgage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
